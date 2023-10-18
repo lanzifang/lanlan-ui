d6c101da21133521a6eae591a2d1dfe0ed6f9dd3
Nancy <1033580067@qq.com>
2023-10-18 12:49:13 +0800

    hover之后右滚

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -16,12 +16,18 @@
         position: relative;
     }
     span{
+        $h:18px;
         position: absolute;
         top: 2px;
         left: 2px;
+        //left: 24px;//经测试，这个数值是最合适的
+        //left: calc(100%-#{$h}-2px);//这个calc用不明白
         height: 18px;
         width: 18px;
         background: white;
         border-radius: 9px;
     } 
+    button:hover>span{
+        left: 24px;
+    }
 </style>
