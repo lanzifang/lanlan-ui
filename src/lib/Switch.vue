d98a5942409192b5729730f5c76ffe558f624565
Nancy <1033580067@qq.com>
2023-10-18 12:29:34 +0800

    Switch css 1

---
+++ src/lib/Switch.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <button>
+            <span></span>
+        </button>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    button{
+        height:22px;
+        width: 44px;
+        border: none;
+        background: blue;
+        border-radius: 11px;
+        position: relative;
+    }
+    span{
+        position: absolute;
+        top: 2px;
+        left: 2px;
+        height: 18px;
+        width: 18px;
+        background: white;
+        border-radius: 9px;
+    } 
+</style>
