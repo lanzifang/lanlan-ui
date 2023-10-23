78778da89127b57ae208dcc63016aecab4ce9b3a
Nancy <1033580067@qq.com>
2023-10-23 16:15:30 +0800

    size css 2

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -12,6 +12,16 @@
         <Button>普普通</Button>
         <Button size="small">小小小</Button>
     </div>
+    <div>
+        <Button theme="link" size="big">大大大</Button>
+        <Button theme="link">普普通</Button>
+        <Button theme="link" size="small">小小小</Button>
+    </div>
+    <div>
+        <Button theme="text" size="big">大大大</Button>
+        <Button theme="text">普普通</Button>
+        <Button theme="text" size="small">小小小</Button>
+    </div>
 </template>
 
 <script lang="ts">
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -84,17 +84,15 @@ export default{
                 background: darken(white, 5%);;
             }
         }
-        &.lanlan-theme-button{
-            &.lanlan-size-big{
-                font-size: 24px;
-                height: 48px;
-                padding: 0 16px
-            }
-            &.lanlan-size-small{
-                font-size: 12px;
-                height: 20px;
-                padding: 0 4px;
-            }
-          }
+        &.lanlan-size-big{
+            font-size: 24px;
+            height: 48px;
+            padding: 0 16px
+        }
+        &.lanlan-size-small{
+            font-size: 12px;
+            height: 20px;
+            padding: 0 4px;
+        }   
     }
 </style>
