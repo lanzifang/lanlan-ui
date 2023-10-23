e35d865176e19156859b1eb8ff8abe9381b50c74
Nancy <1033580067@qq.com>
2023-10-23 10:40:53 +0800

    创建button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,5 +1,12 @@
 <template>
     <div>
-        button示例
+        <Button>你好</Button>
     </div>
 </template>
+
+<script lang="ts">
+import Button from '../lib/Button.vue'
+export default{
+    components:{Button}
+}
+</script>
