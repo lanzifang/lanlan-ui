b1d071af236c301e8462a87ddacb3b7bd305d950
Nancy <1033580067@qq.com>
2023-10-23 10:11:24 +0800

    修改CSS代码

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -29,33 +29,33 @@ export default{
         background:#bfbfbf;
         border-radius: 11px;
         position: relative;
-    }
-    span{
-        $h:18px;
-        position: absolute;
-        top: 2px;
-        left: 2px;
-        //left: 24px;//经测试，这个数值是最合适的
-        //left: calc(100%-#{$h}-2px);//这个calc用不明白
-        height: 18px;
-        width: 18px;
-        background: white;
-        border-radius: 9px;
-        transition: all 250ms;
+        >span{
+            $h:18px;
+            position: absolute;
+            top: 2px;
+            left: 2px;
+            //left: 24px;//经测试，这个数值是最合适的
+            //left: calc(100%-#{$h}-2px);//这个calc用不明白
+            height: 18px;
+            width: 18px;
+            background: white;
+            border-radius: 9px;
+            transition: all 250ms;
+        } 
+        &.checked{
+            background: #1890ff;
+            >span{
+                left: 24px;
+            }
+        }
+        &:focus{
+            outline: none;
+        }
+        &:active{
+            >span{width: 22px;}
+        }
+        &.checked:active{
+            >span{width: 22px;margin-left: -4px;}
+        }
     } 
-    button.checked{
-        background: #1890ff;
-    }
-    button.checked>span{
-        left: 24px;
-    }
-    button:focus{
-        outline: none;
-    }
-    button:active{
-        >span{width: 22px;}
-    }
-    button.checked:active{
-        >span{width: 22px;margin-left: -4px;}
-    }
 </style>
