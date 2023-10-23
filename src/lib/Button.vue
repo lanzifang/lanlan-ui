bec21bb3fccb73997efc1f1310e80695cac65ae6
Nancy <1033580067@qq.com>
2023-10-23 16:36:04 +0800

    支持level属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -16,15 +16,19 @@ export default{
         size:{
             type:String,
             default:'normal'
+        },
+        level:{
+            type:String,
+            default:'normal'
         }
     },
     setup(props){
-        const {theme,size}=props
+        const {theme,size,level}=props
         const classes=computed(()=>{
             return{
                 [`lanlan-theme-${theme}`]:theme,
                 [`lanlan-size-${size}`]:size,
-
+                [`lanlan-level-${level}`]:level,
             }
         })
         return {classes}
@@ -38,6 +42,7 @@ export default{
     $color: #333;
     $blue: #40a9ff;
     $radius: 4px;
+    $red:red;
     .lanlan-button{
         box-sizing: border-box;
         height: $h;
@@ -52,6 +57,7 @@ export default{
         border: 1px solid $border-color;
         border-radius: $radius;
         box-shadow: 0 1px 0 fade-out(black, 0.95);
+        transition: background 250ms;
         & + & {
             margin-left: 8px;
         }
@@ -93,6 +99,53 @@ export default{
             font-size: 12px;
             height: 20px;
             padding: 0 4px;
-        }   
+        }
+        &.lanlan-theme-button {
+          &.lanlan-level-main {
+            background: $blue;
+            color: white;
+            border-color: $blue;
+            &:hover,
+            &:focus {
+              background: darken($blue, 10%);
+              border-color: darken($blue, 10%);
+            }
+          }
+          &.lanlan-level-danger {
+            background: $red;
+            border-color: $red;
+            color: white;
+            &:hover,
+            &:focus {
+              background: darken($red, 10%);
+              border-color: darken($red, 10%);
+            }
+          }
+        } 
+        &.lanlan-theme-link {
+        &.lanlan-level-danger {
+          color: $red;
+          &:hover,
+          &:focus {
+            color: darken($red, 10%);
+          }
+        }
+      }  
+      &.lanlan-theme-text {
+        &.lanlan-level-main {
+          color: $blue;
+          &:hover,
+          &:focus {
+            color: darken($blue, 10%);
+          }
+        }
+        &.lanlan-level-danger {
+          color: $red;
+          &:hover,
+          &:focus {
+            color: darken($red, 10%);
+          }
+        }
+      }
     }
 </style>
