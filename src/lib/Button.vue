22fdb203814a784e6f959c78561fa53659770f23
Nancy <1033580067@qq.com>
2023-10-23 17:46:21 +0800

    加载中CSS

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,6 @@
 <template>
     <button class="lanlan-button" :class="classes" :disabled="disabled">
+        <span v-if="loading" class="lanlan-loadingIndicator"></span>
         <slot />
     </button>
 </template>
@@ -22,6 +23,10 @@ export default{
             default:'normal'
         },
         disabled:{
+            type:Boolean,
+            default:false
+        },
+        loading:{
             type:Boolean,
             default:false
         }
@@ -167,5 +172,20 @@ export default{
           color: $grey;
         }
       }
+      > .lanlan-loadingIndicator{
+        width: 14px;
+        height: 14px;
+        display: inline-block;
+        margin-right: 4px;
+        border-radius: 8px; 
+        border-color: $blue $blue $blue transparent;
+        border-style: solid;
+        border-width: 2px;
+        animation: lanlan-spin 1s infinite linear;
+      }
+    }
+    @keyframes lanlan-spin {
+      0%{transform: rotate(0deg)} 
+      100%{transform: rotate(360deg)} 
     }
 </style>
