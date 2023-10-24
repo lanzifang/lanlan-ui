8c0af2e8185bbe9012c44624bd68397a10200e89
Nancy <1033580067@qq.com>
2023-10-24 10:14:11 +0800

    创建Dialog组件

---
+++ src/lib/Dialog.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="dialog-overlay"></div>
+    <div class="dialog-wrapper">
+        <header>标题</header>
+        <main>
+            <p>内容1</p>
+            <p>内容2</p>
+        </main>
+        <footer>
+            <Button>OK</Button>
+            <Button>Cancel</Button>
+        </footer>
+    </div>
+</template>
+
+<script lang="ts">
+import Button from './Button.vue';
+export default{
+    components:{
+        Button
+    }
+}
+</script>
