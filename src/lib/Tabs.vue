18f1a2f37aabfe4c1ad0312d83379043e0e5b889
Nancy <1033580067@qq.com>
2023-11-01 21:54:09 +0800

    用watchEffect优化onMounted和onUpdated

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -16,7 +16,7 @@
 </template>
 
 <script lang="ts">
-import { computed, onMounted, onUpdated, ref } from 'vue'
+import { computed, onMounted, onUpdated, ref, watchEffect } from 'vue'
 import Tab from './Tab.vue'
 export default{
     props:{
@@ -40,16 +40,16 @@ export default{
         const selectedItem=ref(null)
         const indicator=ref<HTMLDListElement>(null)
         const container=ref<HTMLDListElement>(null)
-        const x=()=>{
-            const {width}=selectedItem.value.getBoundingClientRect()
-            indicator.value.style.width=width+'px'
-            const {left:left1}=container.value.getBoundingClientRect()
-            const {left:left2}=selectedItem.value.getBoundingClientRect()
-            const left= left2-left1
-            indicator.value.style.left=left+'px'
-        }
-        onMounted(x)
-        onUpdated(x)
+        onMounted(()=>{
+            watchEffect(()=>{
+                const {width}=selectedItem.value.getBoundingClientRect()
+                indicator.value.style.width=width+'px'
+                const {left:left1}=container.value.getBoundingClientRect()
+                const {left:left2}=selectedItem.value.getBoundingClientRect()
+                const left= left2-left1
+                indicator.value.style.left=left+'px'
+            })
+        })
         const select=(title:string)=>{
             context.emit('update:selected',title)
         }
