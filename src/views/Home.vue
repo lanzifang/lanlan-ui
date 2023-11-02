bc1366d69bbda0cf18c1c90814ec8f21681a89f3
Nancy <1033580067@qq.com>
2023-11-02 11:00:58 +0800

    改一些字体颜色、从icon引入图标

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="topnavAndBanner">
+    <div>
+        <div class="topnavAndBanner">
         <Topnav />
         <div class="banner">
             <h1>蓝蓝UI</h1>
@@ -9,6 +10,19 @@
                 <router-link to="/doc">开始</router-link>
             </p>
         </div>
+        </div>
+        <div class="features">
+            <svg class="icon">
+                <use xlink:href="#icon-Vue"></use>
+            </svg>
+            <svg class="icon">
+                <use xlink:href="#icon-ts"></use>
+            </svg>
+            <svg class="icon">
+                <use xlink:href="#icon-light"></use>
+            </svg>
+        </div>
+        
     </div>
 </template>
 
@@ -20,10 +34,20 @@
 </script>
 
 <style lang="scss" scoped>
+    $green:#02bcb0;
+    $border-radius:4px;
+    $color:#137f7a;
     .topnavAndBanner{
         background: linear-gradient(145deg, rgba(227,255,253,1) 0%, rgba(183,233,230,1) 100%);
     }
+    .features{
+        >svg{
+            width: 64px;
+            height: 64px;
+        }
+    }
     .banner{
+        color: $color;
         padding: 100px 0;
         display: flex;
         justify-content: center;
@@ -33,13 +57,15 @@
             padding: 8px 0;
             a{
                 margin: 0 8px;
-                background: #fff;
+                background:$green;
+                color: white;
                 display: inline-block;
-                $h:28px;
-                height: $h;
-                line-height: $h;
-                border-radius: $h/2;
-                padding: 0 8px;
+                padding: 8px 24px;
+                border-radius: $border-radius;
+
+                &:hover{
+                    text-decoration: none;
+                }
             }
         }
     }
