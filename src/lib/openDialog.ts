ace7115aba61524dfc07583974ecdfe92943dc13
Nancy <1033580067@qq.com>
2023-11-08 19:58:33 +0800

    改写vite3代码

--- src/lib/openDialog.ts
+++ src/lib/openDialog.ts
@@ -1,25 +1,40 @@
-import Dialog from './Dialog.vue'
-import { createApp,h } from 'vue'
-export const openDialog=(options)=>{
-    const {title,content}=options
-    const div=document.createElement('div')
-    document.body.appendChild(div)
-    const close=()=>{
-        //@ts-ignore
-        app.unmount(div)
-        div.remove()
-    }
-    const app=createApp({
-        render(){
-            return h(Dialog,{visible:true,
-                'onUpdate:visible':(newVisible)=>{
-                    if(newVisible===false){
-                        close()
-                    }
-                }},{
-                title,content
-            })
-        }
-    })
-    app.mount(div)
+import Dialog from "./Dialog.vue";
+import { createApp, h } from "vue";
+
+interface Options {
+  title: string;
+  content?: string;
+  ok?: () => boolean;
+  cancel?: () => void;
 }
+
+export const openDialog = (options: Options) => {
+  const { title, content, ok, cancel } = options;
+  const div = document.createElement("div");
+  document.body.appendChild(div);
+  const close = () => {
+    app.unmount();
+    div.remove();
+  };
+  const app = createApp({
+    render() {
+      return h(
+        Dialog,
+        {
+          visible: true,
+          "onUpdate:visible": (newVisible: boolean) => {
+            if (newVisible === false) {
+              close();
+            }
+          },
+          ok, cancel
+        },
+        {
+          title,
+          content,
+        }
+      );
+    },
+  });
+  app.mount(div);
+};
